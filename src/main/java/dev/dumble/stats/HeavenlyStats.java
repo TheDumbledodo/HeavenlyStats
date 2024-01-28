02a8ff058a719a006dca23308b3cb0841ce8473f
The_Dumbledodo <73080312+TheDumbledodo@users.noreply.github.com>
2024-01-28 18:12:26 +0330

    initial project

---
+++ src/main/java/dev/dumble/stats/HeavenlyStats.java
@@ -0,0 +1,14 @@
+package dev.dumble.stats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HeavenlyStats extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
